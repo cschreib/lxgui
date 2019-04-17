1572a8b735d5d6ee0b1619bbc576ccc72af881ad
Corentin Schreiber <cschreib@orange.fr>
2019-04-17 22:35:05 +0100

    Improved CMake Find script to include dependencies

--- cmake/Modules/Findlxgui.cmake
+++ cmake/Modules/Findlxgui.cmake
@@ -1,7 +1,7 @@
 # Locate the lxgui library
 # This module defines the following variables:
-# LXGUI_LIBRARIES, the name of the library files that one has to link to.
-# LXGUI_INCLUDE_DIR, where to find include files.
+# LXGUI_LIBRARIES, the list of all libraries required to compile against lxgui.
+# LXGUI_INCLUDE_DIRS, the list of all include directories, including dependencies.
 # LXGUI_FOUND, true if both the LXGUI_LIBRARY and LXGUI_INCLUDE_DIR have been found.
 # LXGUI_VERSION_STRING, the version of the library that has been found.
 # LXGUI_IMPL_INCLUDE_DIR, where to find implementation include files.
@@ -15,9 +15,10 @@
 # LXGUI_INPUT_OIS_FOUND, true if both LXGUI_FOUND and LXGUI_INPUT_OIS_LIBRARY have been found.
 #
 
-find_path(LXGUI_INCLUDE_DIR lxgui.hpp DOC "Path to lxgui include directory."
+find_path(LXGUI_INCLUDE_DIR lxgui/lxgui.hpp DOC "Path to lxgui include directory."
     HINTS $ENV{LXGUI_DIR}
-    PATH_SUFFIX include/lxgui PATHS /usr/include/lxgui/ /usr/local/include/lxgui/ ${LXGUI_DIR}/include/lxgui/
+    PATH_SUFFIXES include
+    PATHS /usr/include/ /usr/local/include/ ${LXGUI_DIR}/include/
 )
 
 find_library(LXGUI_LIBRARY DOC "Absolute path to lxgui library."
@@ -96,28 +97,87 @@ find_package_handle_standard_args(LXGUI
                                   REQUIRED_VARS LXGUI_LIBRARY LXGUI_LUAPP_LIBRARY LXGUI_XML_LIBRARY LXGUI_UTILS_LIBRARY LXGUI_INCLUDE_DIR
                                   VERSION_VAR LXGUI_VERSION_STRING)
 
+find_package(Lua51 REQUIRED)
+
 set(LXGUI_GUI_GL_FOUND FALSE)
 set(LXGUI_INPUT_GLFW_FOUND FALSE)
 set(LXGUI_INPUT_SFML_FOUND FALSE)
 set(LXGUI_INPUT_OIS_FOUND FALSE)
 
+set(LXGUI_INCLUDE_DIRS ${LXGUI_INCLUDE_DIR} ${LUA_INCLUDE_DIR})
+set(LXGUI_LIBRARIES ${LXGUI_LIBRARY} ${LXGUI_LUAPP_LIBRARY} ${LXGUI_XML_LIBRARY} ${LXGUI_UTILS_LIBRARY} ${LUA_LIBRARIES})
+
 if(LXGUI_FOUND AND LXGUI_GUI_GL_LIBRARY)
-    set(LXGUI_GUI_GL_FOUND TRUE)
-    message(STATUS "Found lxgui-gl")
+    find_package(Freetype)
+    find_package(PNG)
+    find_package(ZLIB)
+    find_package(GLEW)
+    find_package(OpenGL)
+
+    if(OPENGL_FOUND AND GLEW_FOUND AND FREETYPE_FOUND AND PNG_FOUND AND ZLIB_FOUND)
+        message(STATUS "Found lxgui-gl")
+        set(LXGUI_GUI_GL_FOUND TRUE)
+
+        set(LXGUI_INCLUDE_DIRS ${LXGUI_INCLUDE_DIRS} ${FREETYPE_INCLUDE_DIRS})
+        set(LXGUI_INCLUDE_DIRS ${LXGUI_INCLUDE_DIRS} ${PNG_INCLUDE_DIR})
+        set(LXGUI_INCLUDE_DIRS ${LXGUI_INCLUDE_DIRS} ${ZLIB_INCLUDE_DIR})
+        set(LXGUI_INCLUDE_DIRS ${LXGUI_INCLUDE_DIRS} ${GLEW_INCLUDE_DIR})
+        set(LXGUI_INCLUDE_DIRS ${LXGUI_INCLUDE_DIRS} ${OPENGL_INCLUDE_DIR})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${LXGUI_GUI_GL_LIBRARY})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${FREETYPE_LIBRARY})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${PNG_LIBRARY})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${ZLIB_LIBRARY})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${GLEW_LIBRARY})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${OPENGL_LIBRARY})
+    else()
+        message(ERROR ": the OpenGL implementation of the GUI requires OpenGL, GLEW, freetype, libpng and zlib")
+    endif()
 endif()
 
 if(LXGUI_FOUND AND LXGUI_INPUT_GLFW_LIBRARY)
-    set(LXGUI_INPUT_GLFW_FOUND TRUE)
-    message(STATUS "Found lxgui-input-glfw")
+    find_package(GLFW 2 EXACT)
+
+    if(GLFW_FOUND)
+        message(STATUS "Found lxgui-input-glfw")
+        set(LXGUI_INPUT_GLFW_FOUND TRUE)
+
+        set(LXGUI_INCLUDE_DIRS ${LXGUI_INCLUDE_DIRS} ${GLFW_INCLUDE_DIR})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${LXGUI_INPUT_GLFW_LIBRARY})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${GLFW_LIBRARY})
+    else()
+        message(ERROR ": the GLFW implementation of the input requires the GLFW library")
+    endif()
 endif()
 
 if(LXGUI_FOUND AND LXGUI_INPUT_SFML_LIBRARY)
-    set(LXGUI_INPUT_SFML_FOUND TRUE)
-    message(STATUS "Found lxgui-input-sfml")
+    find_package(SFML 2 COMPONENTS system window)
+
+    if(SFML_FOUND)
+        set(LXGUI_INPUT_SFML_FOUND TRUE)
+        message(STATUS "Found lxgui-input-sfml")
+
+        set(LXGUI_INCLUDE_DIRS ${LXGUI_INCLUDE_DIRS} ${SFML_INCLUDE_DIR})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${LXGUI_INPUT_SFML_LIBRARY})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${SFML_WINDOW_LIBRARY})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${SFML_SYSTEM_LIBRARY})
+    else()
+        message(ERROR ": the SFML implementation of the input requires the SFML library")
+    endif()
 endif()
 
 if(LXGUI_FOUND AND LXGUI_INPUT_OIS_LIBRARY)
-    set(LXGUI_INPUT_OIS_FOUND TRUE)
-    message(STATUS "Found lxgui-input-ois")
+    find_package(OIS)
+
+    if(OIS_FOUND)
+        set(LXGUI_INPUT_OIS_FOUND TRUE)
+        message(STATUS "Found lxgui-input-ois")
+
+        set(LXGUI_INCLUDE_DIRS ${LXGUI_INCLUDE_DIRS} ${OIS_INCLUDE_DIR})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${LXGUI_INPUT_OIS_LIBRARY})
+        set(LXGUI_LIBRARIES ${LXGUI_LIBRARIES} ${OIS_LIBRARY})
+    else()
+        message(ERROR ": the OIS implementation of the input requires the OIS library")
+    endif()
 endif()
 
+mark_as_advanced(LXGUI_INCLUDE_DIRS LXGUI_LIBRARIES)
