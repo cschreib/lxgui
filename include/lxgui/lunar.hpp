311325e1e59670b76fa092dfb17f66a307a3a23e
Corentin Schreiber <cschreib@orange.fr>
2020-03-08 18:05:30 +0000

    Added explicit static_cast<void*>()

--- include/lxgui/lunar.hpp
+++ include/lxgui/lunar.hpp
@@ -234,7 +234,7 @@ private:
     char buff[32];
     userdataType *ud = static_cast<userdataType*>(lua_touserdata(L, 1));
     T *obj = ud->pT;
-    sprintf(buff, "%p", obj);
+    sprintf(buff, "%p", static_cast<void*>(obj));
     lua_pushfstring(L, "%s (%s)", T::className, buff);
     return 1;
   }
