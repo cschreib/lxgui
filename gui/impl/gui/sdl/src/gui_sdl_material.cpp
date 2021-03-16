845f1ac809922e2b5c1c56ada173e1ddb2f4d6aa
Corentin Schreiber <cschreib@orange.fr>
2021-03-16 21:42:38 +0000

    Work in progress on SDL material

--- gui/impl/gui/sdl/src/gui_sdl_material.cpp
+++ gui/impl/gui/sdl/src/gui_sdl_material.cpp
@@ -4,11 +4,309 @@
 #include <lxgui/gui_exception.hpp>
 #include <lxgui/utils_string.hpp>
 
+#include <SDL.h>
+#include <SDL_image.h>
+
 namespace lxgui {
 namespace gui {
 namespace sdl
 {
 
+void initialise_SDL_image()
+{
+    static bool bSDLImageInitialised = false;
+
+    if (bSDLImageInitialised) return;
+
+    int iImgFlags = IMG_INIT_PNG;
+    if ((IMG_Init(iImgFlags) & iImgFlags) == 0)
+    {
+        throw gui::exception("gui::sdl::material", "Could not initialise SDL_image: "+
+            std::string(IMG_GetError())+".");
+    }
+
+    bSDLImageInitialised = true;
+}
+
+material::material(SDL_Renderer* pRenderer, uint uiWidth, uint uiHeight, bool bRenderTarget, wrap mWrap, filter mFilter)
+{
+    SDL_RendererInfo mInfo;
+    SDL_GetRendererInfo(pRenderer, &mInfo);
+
+    if (uiWidth > (uint)mInfo.max_texture_width || uiHeight > (uint)mInfo.max_texture_height)
+    {
+        throw gui::exception("gui::sdl::material", "Texture dimensions not supported by hardware: ("+
+            utils::to_string(uiWidth)+" x "+utils::to_string(uiHeight)+").");
+    }
+
+    auto& mTexData = mData_.emplace<texture_data>();
+
+    mTexData.pTexture_ = SDL_CreateTexture(pRenderer,
+        SDL_PIXELFORMAT_RGBA8888,
+        bRenderTarget ? SDL_TEXTUREACCESS_TARGET : SDL_TEXTUREACCESS_STREAMING,
+        uiWidth, uiHeight);
+
+    if (mTexData.pTexture_ == nullptr)
+    {
+        throw gui::exception("gui::sdl::material", "Could not create "+
+            std::string(bRenderTarget ? "render target" : "texture")+" with dimensions "+
+            utils::to_string(uiWidth)+" x "+utils::to_string(uiHeight)+".");
+    }
+
+    mTexData.uiWidth_ = uiWidth;
+    mTexData.uiHeight_ = uiHeight;
+    mTexData.mWrap_ = mWrap;
+    mTexData.mFilter_ = mFilter;
+    mTexData.uiRealWidth_ = uiWidth;
+    mTexData.uiRealHeight_ = uiHeight;
+    mTexData.bRenderTarget_ = bRenderTarget;
+}
+
+material::material(SDL_Renderer* pRenderer, SDL_Surface* pSurface, wrap mWrap, filter mFilter)
+{
+    const uint uiWidth  = pSurface->w;
+    const uint uiHeight = pSurface->h;
+
+    auto& mTexData = mData_.emplace<texture_data>();
+
+    mTexData.pTexture_ = SDL_CreateTextureFromSurface(pRenderer, pSurface);
+    if (mTexData.pTexture_ == nullptr)
+    {
+        throw gui::exception("gui::sdl::material", "Could not create texture with dimensions "+
+            utils::to_string(uiWidth)+" x "+utils::to_string(uiHeight)+".");
+    }
+
+    mTexData.uiWidth_ = uiWidth;
+    mTexData.uiHeight_ = uiHeight;
+    mTexData.mWrap_ = mWrap;
+    mTexData.mFilter_ = mFilter;
+    mTexData.uiRealWidth_ = uiWidth;
+    mTexData.uiRealHeight_ = uiHeight;
+    mTexData.bRenderTarget_ = false;
+}
+
+material::material(SDL_Renderer* pRenderer, const std::string& sFileName, wrap mWrap, filter mFilter)
+{
+    initialise_SDL_image();
+
+    auto& mTexData = mData_.emplace<texture_data>();
+
+    // Load file
+    SDL_Surface* pSurface = IMG_Load(sFileName.c_str());
+    if (pSurface == nullptr)
+    {
+        throw gui::exception("gui::sdl::material", "Could not load image file "+sFileName+".");
+    }
+
+    // Convert to RGBA 32bit
+    SDL_Surface* pConvertedSurface = SDL_ConvertSurfaceFormat(pSurface, SDL_PIXELFORMAT_RGBA8888, 0);
+    SDL_FreeSurface(pSurface);
+    if (pConvertedSurface == NULL)
+    {
+        throw gui::exception("gui::sdl::material", "Could convert image file "+
+            sFileName+" to RGBA format.");
+    }
+
+    // Pre-multiply alpha
+    premultiply_alpha(pConvertedSurface);
+
+    const uint uiWidth  = pSurface->w;
+    const uint uiHeight = pSurface->h;
+
+    // Create streamable texture
+    mTexData.pTexture_ = SDL_CreateTexture(pRenderer, SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_STREAMING, uiWidth, uiHeight);
+    if (mTexData.pTexture_ == nullptr)
+    {
+        SDL_FreeSurface(pConvertedSurface);
+        throw gui::exception("gui::sdl::material", "Could not create texture with dimensions "+
+            utils::to_string(uiWidth)+" x "+utils::to_string(uiHeight)+".");
+    }
+
+    // Copy data into the texture
+    void* pPixelData = nullptr;
+    int iPitch = 0;
+    SDL_LockTexture(mTexData.pTexture_, nullptr, &pPixelData, &iPitch);
+
+    const uint uiSizeInBytes = pConvertedSurface->pitch * pConvertedSurface->h;
+    const std::byte* pSurfacePixelsStart =
+        reinterpret_cast<const std::byte*>(pConvertedSurface->pixels);
+    const std::byte* pSurfacePixelsEnd = pSurfacePixelsStart + uiSizeInBytes;
+    std::byte* pTexturePixels = reinterpret_cast<std::byte*>(pPixelData);
+    std::copy(pSurfacePixelsStart, pSurfacePixelsEnd, pTexturePixels);
+
+    SDL_UnlockTexture(mTexData.pTexture_);
+
+    mTexData.uiWidth_ = uiWidth;
+    mTexData.uiHeight_ = uiHeight;
+    mTexData.mWrap_ = mWrap;
+    mTexData.mFilter_ = mFilter;
+    mTexData.uiRealWidth_ = uiWidth;
+    mTexData.uiRealHeight_ = uiHeight;
+    mTexData.bRenderTarget_ = false;
+}
+
+
+material::material(SDL_Renderer* /*pRenderer*/, const color& mColor)
+{
+    auto& mColData = mData_.emplace<color_data>();
+    mColData.mColor_ = mColor;
+}
+
+material::~material() noexcept
+{
+    if (std::holds_alternative<texture_data>(mData_))
+    {
+        SDL_DestroyTexture(std::get<texture_data>(mData_).pTexture_);
+    }
+}
+
+material::type material::get_type() const
+{
+    return std::holds_alternative<texture_data>(mData_) ? type::TEXTURE : type::COLOR;
+}
+
+color material::get_color() const
+{
+    return std::get<color_data>(mData_).mColor_;
+}
+
+void material::set_wrap(wrap mWrap)
+{
+    if (!std::holds_alternative<texture_data>(mData_)) return;
+
+    std::get<texture_data>(mData_).mWrap_ = mWrap;
+}
+
+void material::set_filter(filter mFilter)
+{
+    if (!std::holds_alternative<texture_data>(mData_)) return;
+
+    std::get<texture_data>(mData_).mFilter_ = mFilter;
+}
+
+void material::premultiply_alpha(SDL_Surface* pSurface)
+{
+    ub32color* pPixelData = reinterpret_cast<ub32color*>(pSurface->pixels);
+
+    const uint uiArea = pSurface->w * pSurface->h;
+    for (uint i = 0; i < uiArea; ++i)
+    {
+        float a = pPixelData[i].a/255.0f;
+        pPixelData[i].r *= a;
+        pPixelData[i].g *= a;
+        pPixelData[i].b *= a;
+    }
+}
+
+float material::get_width() const
+{
+    if (std::holds_alternative<texture_data>(mData_))
+    {
+        return std::get<texture_data>(mData_).uiWidth_;
+    }
+    else
+    {
+        return 1.0f;
+    }
+}
+
+float material::get_height() const
+{
+    if (std::holds_alternative<texture_data>(mData_))
+    {
+        return std::get<texture_data>(mData_).uiHeight_;
+    }
+    else
+    {
+        return 1.0f;
+    }
+}
+
+float material::get_real_width() const
+{
+    if (std::holds_alternative<texture_data>(mData_))
+    {
+        return std::get<texture_data>(mData_).uiRealWidth_;
+    }
+    else
+    {
+        return 1.0f;
+    }
+}
+
+float material::get_real_height() const
+{
+    if (std::holds_alternative<texture_data>(mData_))
+    {
+        return std::get<texture_data>(mData_).uiRealHeight_;
+    }
+    else
+    {
+        return 1.0f;
+    }
+}
+
+bool material::set_dimensions(uint uiWidth, uint uiHeight)
+{
+    if (!std::holds_alternative<texture_data>(mData_)) return false;
+
+    auto& mTexData = std::get<texture_data>(mData_);
+    if (!mTexData.bRenderTarget_) return false;
+
+    // TODO: implement this
+
+    // SDL_RendererInfo mInfo;
+    // SDL_GetRendererInfo(pRenderer, &mInfo);
+
+    // if (uiWidth > (uint)mInfo.max_texture_width || uiHeight > (uint)mInfo.max_texture_height)
+    // {
+    //     return false;
+    // }
+
+    if (uiWidth > mTexData.uiRealWidth_ || uiHeight > mTexData.uiRealHeight_)
+    {
+        // mTexData.uiWidth_      = uiWidth;
+        // mTexData.uiHeight_     = uiHeight;
+        // // sdl is not efficient at resizing render texture, so use an exponential growth pattern
+        // // to avoid re-allocating a new render texture on every resize operation.
+        // if (uiWidth > mTexData.uiRealWidth_)
+        //     mTexData.uiRealWidth_  = uiWidth + uiWidth/2;
+        // if (uiHeight > mTexData.uiRealHeight_)
+        //     mTexData.uiRealHeight_ = uiHeight + uiHeight/2;
+
+        // if (!mTexData.mRenderTexture_.create(mTexData.uiRealWidth_, mTexData.uiRealHeight_))
+        // {
+        //     throw gui::exception("gui::sdl::material", "Could not create render target with dimensions "+
+        //         utils::to_string(mTexData.uiRealWidth_)+" x "+utils::to_string(mTexData.uiRealHeight_)+".");
+        // }
+
+        return true;
+    }
+    else
+    {
+        mTexData.uiWidth_  = uiWidth;
+        mTexData.uiHeight_ = uiHeight;
+        return false;
+    }
+}
+
+SDL_Texture* material::get_render_texture()
+{
+    if (!std::holds_alternative<texture_data>(mData_)) return nullptr;
+
+    auto& mTexData = std::get<texture_data>(mData_);
+    if (!mTexData.bRenderTarget_) return nullptr;
+
+    return mTexData.pTexture_;
+}
+
+const SDL_Texture* material::get_texture() const
+{
+    if (!std::holds_alternative<texture_data>(mData_)) return nullptr;
+
+    return std::get<texture_data>(mData_).pTexture_;
+}
 }
 }
 }
--- include/lxgui/impl/gui_sdl_material.hpp
+++ include/lxgui/impl/gui_sdl_material.hpp
@@ -7,12 +7,171 @@
 
 #include <vector>
 #include <memory>
+#include <variant>
+
+struct SDL_Renderer;
+struct SDL_Surface;
+struct SDL_Texture;
 
 namespace lxgui {
 namespace gui {
 namespace sdl
 {
+    struct ub32color
+    {
+        using chanel = unsigned char;
 
+        ub32color() = default;
+        ub32color(chanel tr, chanel tg, chanel tb, chanel ta);
+        chanel r, g, b, a;
+    };
+
+    /// A class that holds rendering data
+    /** This implementation can contain either a plain color
+    *   or a real SDL_Texture. It is also used by the
+    *   gui::sdl::render_target class to store the output data.
+    */
+    class material : public gui::material
+    {
+    public :
+
+        /// Constructor for textures.
+        /** \param uiWidth       The requested texture width
+        *   \param uiHeight      The requested texture height
+        *   \param bRenderTarget Create the material for a render target or only for display
+        *   \param mWrap         How to adjust texture coordinates that are outside the [0,1] range
+        *   \param mFilter       Use texture filtering or not (see set_filter())
+        */
+        material(SDL_Renderer* pRenderer, uint uiWidth, uint uiHeight, bool bRenderTarget, wrap mWrap = wrap::REPEAT,
+            filter mFilter = filter::NONE);
+
+        /// Constructor for textures.
+        /** \param pData         The surface data to use as texture
+        *   \param mWrap         How to adjust texture coordinates that are outside the [0,1] range
+        *   \param mFilter       Use texture filtering or not (see set_filter())
+        */
+        material(SDL_Renderer* pRenderer, SDL_Surface* pSurface, wrap mWrap = wrap::REPEAT,
+            filter mFilter = filter::NONE);
+
+        /// Constructor for textures.
+        /** \param sFileName     The file from which the texture data is loaded
+        *   \param mWrap         How to adjust texture coordinates that are outside the [0,1] range
+        *   \param mFilter       Use texture filtering or not (see set_filter())
+        */
+        material(SDL_Renderer* pRenderer, const std::string& sFileName, wrap mWrap = wrap::REPEAT,
+            filter mFilter = filter::NONE);
+
+        /// Constructor for plain colors.
+        /** \param mColor The plain color to use
+        */
+        material(SDL_Renderer* pRenderer, const color& mColor);
+
+        material(const material& tex) = delete;
+        material(material&& tex) = delete;
+        material& operator = (const material& tex) = delete;
+        material& operator = (material&& tex) = delete;
+
+        /// Destructor.
+        ~material() noexcept override;
+
+        /// Returns the type of this texture (texture or color).
+        /** \return The type of this texture (texture or color)
+        */
+        type get_type() const;
+
+        /// Returns the width of the underlying texture (if any).
+        /** \return The width of the underlying texture (if any)
+        */
+        float get_width() const override;
+
+        /// Returns the height of the underlying texture (if any).
+        /** \return The height of the underlying texture (if any)
+        */
+        float get_height() const override;
+
+        /// Returns the physical width of the underlying texture (if any).
+        /** \return The physical width of the underlying texture (if any)
+        *   \note Some old hardware don't support textures that have non
+        *         power of two dimensions. If the user creates such a material
+        *         and its hardware doesn't support it, this class creates a
+        *         bigger texture that has power of two dimensions (the
+        *         "physical" dimensions).
+        */
+        float get_real_width() const override;
+
+        /// Returns the physical height of the underlying texture (if any).
+        /** \return The physical height of the underlying texture (if any)
+        *   \note Some old hardware don't support textures that have non
+        *         power of two dimensions. If the user creates such a material
+        *         and its hardware doesn't support it, this class creates a
+        *         bigger texture that has power of two dimensions (the
+        *         "physical" dimensions).
+        */
+        float get_real_height() const override;
+
+        /// Resizes this texture.
+        /** \param uiWidth  The new texture width
+        *   \param uiHeight The new texture height
+        *   \return 'true' if the function had to re-create a new texture object
+        *   \note All the previous data that was stored in this texture will be lost.
+        */
+        bool set_dimensions(uint uiWidth, uint uiHeight);
+
+        /// Returns the plain color of this texture.
+        /** \return The plain color of this texture
+        */
+        color get_color() const;
+
+        /// Premultiplies an image by its alpha component.
+        /** \note Premultiplied alpha is a rendering technique that allows perfect
+        *         alpha blending when using render targets.
+        */
+        static void premultiply_alpha(SDL_Surface* pData);
+
+        /// Sets the wrap mode of this texture.
+        /** \param mWrap How to adjust texture coordinates that are outside the [0,1] range
+        */
+        void set_wrap(wrap mWrap);
+
+        /// Sets the filter mode of this texture.
+        /** \param mFilter Use texture filtering or not
+        *   \note When texture filtering is disabled, enlarged textures get pixelated.
+        *         Else, the GPU uses an averaging algorithm to blur the pixels.
+        */
+        void set_filter(filter mFilter);
+
+        /// Returns the underlying SDL texture object.
+        /** return The underlying SDL texture object
+        */
+        const SDL_Texture* get_texture() const;
+
+        /// Returns the underlying SDL texture object (for render target).
+        /** return The underlying SDL texture object (for render target)
+        */
+        SDL_Texture* get_render_texture();
+
+    private:
+
+        struct texture_data
+        {
+            uint   uiWidth_ = 0u, uiHeight_ = 0u;
+            uint   uiRealWidth_ = 0u, uiRealHeight_ = 0u;
+            wrap   mWrap_ = wrap::REPEAT;
+            filter mFilter_ = filter::NONE;
+            bool   bRenderTarget_ = false;
+
+            SDL_Texture* pTexture_ = nullptr;
+        };
+
+        struct color_data
+        {
+            color mColor_;
+        };
+
+        struct empty {};
+
+        std::variant<empty,texture_data,color_data> mData_;
+    };
 }
 }
 }
