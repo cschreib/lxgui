5557f7e06bacae1ece8dea2010b6498ab90726cc
Corentin Schreiber <cschreib@orange.fr>
2021-03-17 08:39:05 +0000

    Added SDL render target implementation

--- gui/impl/gui/sdl/src/gui_sdl_rendertarget.cpp
+++ gui/impl/gui/sdl/src/gui_sdl_rendertarget.cpp
@@ -1,12 +1,88 @@
 #include "lxgui/impl/gui_sdl_rendertarget.hpp"
 #include "lxgui/impl/gui_sdl_renderer.hpp"
+#include <lxgui/gui_exception.hpp>
 
 #include <iostream>
+
+#include <SDL.h>
 
 namespace lxgui {
 namespace gui {
 namespace sdl
 {
+render_target::render_target(SDL_Renderer* pRenderer, uint uiWidth, uint uiHeight)
+{
+    pTexture_ = std::make_shared<sdl::material>(
+        pRenderer, uiWidth, uiHeight, true, material::wrap::REPEAT, material::filter::NONE
+    );
+}
+
+void render_target::begin()
+{
+    if (SDL_SetRenderTarget(pTexture_->get_renderer(), pTexture_->get_render_texture()) != 0)
+    {
+        throw gui::exception("Could not set current render target");
+    }
+}
+
+void render_target::end()
+{
+    SDL_SetRenderTarget(pTexture_->get_renderer(), nullptr);
+}
+
+void render_target::clear(const color& mColor)
+{
+    SDL_SetRenderDrawColor(pTexture_->get_renderer(),
+        mColor.r*255, mColor.g*255, mColor.b*255, mColor.a*255);
+
+    SDL_RenderClear(pTexture_->get_renderer());
+}
+
+uint render_target::get_width() const
+{
+    return pTexture_->get_width();
+}
+
+uint render_target::get_height() const
+{
+    return pTexture_->get_height();
+}
+
+uint render_target::get_real_width() const
+{
+    return pTexture_->get_real_width();
+}
+
+uint render_target::get_real_height() const
+{
+    return pTexture_->get_real_height();
+}
+
+bool render_target::set_dimensions(uint uiWidth, uint uiHeight)
+{
+    return pTexture_->set_dimensions(uiWidth, uiHeight);
+}
+
+std::weak_ptr<sdl::material> render_target::get_material()
+{
+    return pTexture_;
+}
+
+SDL_Texture* render_target::get_render_texture()
+{
+    return pTexture_->get_render_texture();
+}
+
+void render_target::check_availability(SDL_Renderer* pRenderer)
+{
+    SDL_RendererInfo mInfo;
+    SDL_GetRendererInfo(pRenderer, &mInfo);
+
+    if ((mInfo.flags & SDL_RENDERER_TARGETTEXTURE) == 0)
+    {
+        throw gui::exception("gui::sdl::material", "Render targets are not supported by hardware.");
+    }
+}
 
 }
 }
--- include/lxgui/impl/gui_sdl_rendertarget.hpp
+++ include/lxgui/impl/gui_sdl_rendertarget.hpp
@@ -8,11 +8,93 @@
 
 #include <memory>
 
+struct SDL_Renderer;
+struct SDL_Texture;
+
 namespace lxgui {
 namespace gui {
-namespace sfml
+namespace sdl
 {
+    /// A place to render things (the screen, a texture, ...)
+    class render_target : public gui::render_target
+    {
+    public :
 
+        /// Constructor.
+        /** \param uiWidth  The width of the render_target
+        *   \param uiHeight The height of the render_target
+        */
+        render_target(SDL_Renderer* pRenderer, uint uiWidth, uint uiHeight);
+
+        /// Begins rendering on this target.
+        void begin() override;
+
+        /// Ends rendering on this target.
+        void end() override;
+
+        /// Clears the content of this render_target.
+        /** \param mColor The color to use as background
+        */
+        void clear(const color& mColor) override;
+
+        /// Returns this render target's width.
+        /** \return This render target's width
+        */
+        uint get_width() const override;
+
+        /// Returns this render target's height.
+        /** \return This render target's height
+        */
+        uint get_height() const override;
+
+        /// Sets this render target's dimensions.
+        /** \param uiWidth This render target's width
+        *   \param uiHeight This render target's height
+        *   \return 'true' if the function had to re-create a
+        *           new render target
+        */
+        bool set_dimensions(uint uiWidth, uint uiHeight) override;
+
+        /// Returns this render target's real width.
+        /** \return This render target's real width
+        *   \note This is the physical size of the render target.
+        *         On some systems, abitrary dimensions are not supported :
+        *         they can be promoted to the nearest power of two from
+        *         for example.
+        */
+        uint get_real_width() const override;
+
+        /// Returns this render target's real height.
+        /** \return This render target's real height
+        *   \note This is the physical size of the render target.
+        *         On some systems, abitrary dimensions are not supported :
+        *         they can be promoted to the nearest power of two from
+        *         for example.
+        */
+        uint get_real_height() const override;
+
+        /// Returns the associated texture for rendering.
+        /** \return The underlying pixel buffer, that you can use to render its content
+        */
+        std::weak_ptr<sdl::material> get_material();
+
+        /// Checks if the machine is capable of using render targets.
+        /** \param pRenderer The renderer to check for availability
+        *   \note If not, this function throws a gui::exception.
+        */
+        static void check_availability(SDL_Renderer* pRenderer);
+
+        /// Returns the underlying SDL render texture object.
+        /** return The underlying SDL render texture object
+        */
+        SDL_Texture* get_render_texture();
+
+    private :
+
+        void update_view_matrix_() const;
+
+        std::shared_ptr<sdl::material> pTexture_;
+    };
 }
 }
 }
