ffecd6e23b6945e65ae000dbc1ed983c04aec3e2
Corentin Schreiber <cschreib@orange.fr>
2021-03-19 21:37:43 +0000

    Implemented support for gradients in SDL renderer

--- gui/impl/gui/sdl/src/gui_sdl_renderer.cpp
+++ gui/impl/gui/sdl/src/gui_sdl_renderer.cpp
@@ -39,6 +39,31 @@ void renderer::end() const
     pCurrentTarget_ = nullptr;
 }
 
+color premultiply_alpha(const color& mColor)
+{
+    return color(mColor.r*mColor.a, mColor.g*mColor.a, mColor.b*mColor.a, mColor.a);
+}
+
+color interpolate_color(const color& mColor1, const color& mColor2, float fInterp)
+{
+    return color(
+        mColor1.r*(1.0f - fInterp) + mColor2.r*fInterp,
+        mColor1.g*(1.0f - fInterp) + mColor2.g*fInterp,
+        mColor1.b*(1.0f - fInterp) + mColor2.b*fInterp,
+        mColor1.a*(1.0f - fInterp) + mColor2.a*fInterp
+    );
+}
+
+ub32color to_ub32color(const color& mColor)
+{
+    return {
+        static_cast<unsigned char>(mColor.r*255),
+        static_cast<unsigned char>(mColor.g*255),
+        static_cast<unsigned char>(mColor.b*255),
+        static_cast<unsigned char>(mColor.a*255)
+    };
+}
+
 void renderer::render_quad(const quad& mQuad) const
 {
     std::shared_ptr<sdl::material> pMat = std::static_pointer_cast<sdl::material>(mQuad.mat);
@@ -64,25 +89,64 @@ void renderer::render_quad(const quad& mQuad) const
             static_cast<int>((mQuad.v[2].uvs.y - mQuad.v[0].uvs.y)*fTexHeight)
         };
 
-        // Note: SDL does not support per-vertex color; just use the first vertex.
-        const auto mColor = mQuad.v[0].col;
+        if (mQuad.v[0].col == mQuad.v[1].col && mQuad.v[0].col == mQuad.v[2].col &&
+            mQuad.v[0].col == mQuad.v[3].col)
+        {
+            const auto mColor = mQuad.v[0].col;
 
-        SDL_SetTextureColorMod(pMat->get_texture(),
-            mColor.r*mColor.a*255, mColor.g*mColor.a*255, mColor.b*mColor.a*255);
-        SDL_SetTextureAlphaMod(pMat->get_texture(), mColor.a*255);
+            SDL_SetTextureColorMod(pMat->get_texture(),
+                mColor.r*mColor.a*255, mColor.g*mColor.a*255, mColor.b*mColor.a*255);
+            SDL_SetTextureAlphaMod(pMat->get_texture(), mColor.a*255);
 
-        SDL_RenderCopy(pRenderer_, pMat->get_texture(), &mSrcQuad, &mDestQuad);
+            SDL_RenderCopy(pRenderer_, pMat->get_texture(), &mSrcQuad, &mDestQuad);
+        }
+        else
+        {
+            throw gui::exception("sdl::renderer", "Per-vertex color with texture is not supported.");
+        }
     }
     else
     {
-        // Note: SDL does not support per-vertex color; just use the first vertex.
-        const auto& mColor = mQuad.v[0].col * pMat->get_color();
-        SDL_SetRenderDrawBlendMode(pRenderer_,
-            (SDL_BlendMode)material::get_premultiplied_alpha_blend_mode());
-        SDL_SetRenderDrawColor(pRenderer_,
-            mColor.r*mColor.a*255, mColor.g*mColor.a*255, mColor.b*mColor.a*255, mColor.a*255);
+        if (mQuad.v[0].col == mQuad.v[1].col && mQuad.v[0].col == mQuad.v[2].col &&
+            mQuad.v[0].col == mQuad.v[3].col)
+        {
+            // Same color for all vertices
+            const auto& mColor = mQuad.v[0].col * pMat->get_color();
+            SDL_SetRenderDrawBlendMode(pRenderer_,
+                (SDL_BlendMode)material::get_premultiplied_alpha_blend_mode());
+            SDL_SetRenderDrawColor(pRenderer_,
+                mColor.r*mColor.a*255, mColor.g*mColor.a*255, mColor.b*mColor.a*255, mColor.a*255);
 
-        SDL_RenderFillRect(pRenderer_, &mDestQuad);
+            SDL_RenderFillRect(pRenderer_, &mDestQuad);
+        }
+        else
+        {
+            // Different colors for each vertex; SDL does not support this natively.
+            // We have to create a temporary texture, do the bilinear interpolation ourselves,
+            // and draw that.
+            const color lColorQuad[4] = {
+                premultiply_alpha(mQuad.v[0].col * pMat->get_color()),
+                premultiply_alpha(mQuad.v[1].col * pMat->get_color()),
+                premultiply_alpha(mQuad.v[2].col * pMat->get_color()),
+                premultiply_alpha(mQuad.v[3].col * pMat->get_color())
+            };
+
+            sdl::material pTempMat(pRenderer_, mDestQuad.w, mDestQuad.h, false);
+            ub32color* pPixelData = pTempMat.lock_pointer();
+            for (int y = 0; y < mDestQuad.h; ++y)
+            for (int x = 0; x < mDestQuad.w; ++x)
+            {
+                const color lColY1 = interpolate_color(lColorQuad[0], lColorQuad[3], y/float(mDestQuad.h - 1));
+                const color lColY2 = interpolate_color(lColorQuad[1], lColorQuad[2], y/float(mDestQuad.h - 1));
+                pPixelData[y * mDestQuad.w + x] = to_ub32color(
+                    interpolate_color(lColY1, lColY2, x/float(mDestQuad.w - 1)));
+            }
+            pTempMat.unlock_pointer();
+
+            const SDL_Rect mSrcQuad = { 0, 0, mDestQuad.w, mDestQuad.h };
+
+            SDL_RenderCopy(pRenderer_, pTempMat.get_texture(), &mSrcQuad, &mDestQuad);
+        }
     }
 }
 
