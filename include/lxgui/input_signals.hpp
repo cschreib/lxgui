7e889b462d9336cb51e11b9ea65fbdf9205e8991
Corentin Schreiber <github@corentin.net>
2022-02-28 08:57:39 +0000

    Added more documentation on input signals

--- include/lxgui/input_signals.hpp
+++ include/lxgui/input_signals.hpp
@@ -19,25 +19,82 @@ public:
     signals& operator=(const signals&) = delete;
     signals& operator=(signals&&) = delete;
 
-    /// Signal triggered when the mouse moves
+    /**
+     * \brief Signal triggered when the mouse moves
+     * \details Arguments:
+     *  - mouse motion that generated this event, in points
+     *  - mouse position, in points
+     */
     utils::signal<void(const gui::vector2f&, const gui::vector2f&)> on_mouse_moved;
-    /// Signal triggered when the mouse wheel is moved
+
+    /**
+     * \brief Signal triggered when the mouse wheel is moved
+     * \details Arguments:
+     *  - mouse wheel motion that generated this event
+     *  - mouse position, in points
+     */
     utils::signal<void(float, const gui::vector2f&)> on_mouse_wheel;
-    /// Signal triggered when a mouse button is pressed
+
+    /**
+     * \brief Signal triggered when a mouse button is pressed
+     * \details Arguments:
+     *  - mouse button that generated this event
+     *  - mouse position, in points
+     */
     utils::signal<void(input::mouse_button, const gui::vector2f&)> on_mouse_pressed;
-    /// Signal triggered when a mouse button is released
+
+    /**
+     * \brief Signal triggered when a mouse button is released
+     * \details Arguments:
+     *  - mouse button that generated this event
+     *  - mouse position, in points
+     */
     utils::signal<void(input::mouse_button, const gui::vector2f&)> on_mouse_released;
-    /// Signal triggered when a mouse button is double clicked
+
+    /**
+     * \brief Signal triggered when a mouse button is double clicked
+     * \details Arguments:
+     *  - mouse button that generated this event
+     *  - mouse position, in points
+     */
     utils::signal<void(input::mouse_button, const gui::vector2f&)> on_mouse_double_clicked;
-    /// Signal triggered when the mouse starts a drag operation
+
+    /**
+     * \brief Signal triggered when the mouse starts a drag operation
+     * \details Arguments:
+     *  - mouse button that is pressed (if more than one, only the first pressed)
+     *  - mouse position, in points
+     */
     utils::signal<void(input::mouse_button, const gui::vector2f&)> on_mouse_drag_start;
-    /// Signal triggered when the mouse ends a drag operation
+
+    /**
+     * \brief Signal triggered when the mouse ends a drag operation
+     * \details Arguments:
+     *  - mouse button that was pressed (if more than one, only the first pressed)
+     *  - mouse position, in points
+     */
     utils::signal<void(input::mouse_button, const gui::vector2f&)> on_mouse_drag_stop;
-    /// Signal triggered when a keyboard key is pressed
+
+    /**
+     * \brief Signal triggered when a keyboard key is pressed
+     * \details Arguments:
+     *  - keyboard key that generated this event
+     */
     utils::signal<void(input::key)> on_key_pressed;
-    /// Signal triggered when a keyboard key is released
+
+    /**
+     * \brief Signal triggered when a keyboard key is released
+     * \details Arguments:
+     *  - keyboard key that generated this event
+     */
     utils::signal<void(input::key)> on_key_released;
-    /// Signal triggered when text is entered
+
+    /**
+     * \brief Signal triggered when text is entered
+     * \details Arguments:
+     *  - Unicode UTF-32 code point of the typed character
+     * \note The event will trigger repeatedly if more than one character is generated.
+     */
     utils::signal<void(std::uint32_t)> on_text_entered;
 };
 
