6dc2b9502838a24d6731698db7ef22a062518b48
Corentin Schreiber <github@corentin.net>
2022-04-09 11:21:19 +0100

    Added missing vertexCacheStrategy to font_string parser

--- src/gui_font_string_parser.cpp
+++ src/gui_font_string_parser.cpp
@@ -29,10 +29,8 @@ void font_string::parse_attributes_(const layout_node& node) {
 
     if (const layout_attribute* attr = node.try_get_attribute("nonSpaceWrap"))
         set_non_space_wrap_enabled(attr->get_value<bool>());
-
     if (const layout_attribute* attr = node.try_get_attribute("spacing"))
         set_spacing(attr->get_value<float>());
-
     if (const layout_attribute* attr = node.try_get_attribute("lineSpacing"))
         set_line_spacing(attr->get_value<float>());
 
@@ -49,35 +47,12 @@ void font_string::parse_attributes_(const layout_node& node) {
         }
     }
 
-    if (const layout_attribute* attr = node.try_get_attribute("alignX")) {
-        const std::string& align_x = attr->get_value<std::string>();
-        if (align_x == "LEFT")
-            set_alignment_x(alignment_x::left);
-        else if (align_x == "CENTER")
-            set_alignment_x(alignment_x::center);
-        else if (align_x == "RIGHT")
-            set_alignment_x(alignment_x::right);
-        else {
-            gui::out << gui::warning << node.get_location() << ": "
-                     << "Unknown horizontal alignment behavior for " << name_ << ": \"" << align_x
-                     << "\"." << std::endl;
-        }
-    }
-
-    if (const layout_attribute* attr = node.try_get_attribute("alignY")) {
-        const std::string& align_y = attr->get_value<std::string>();
-        if (align_y == "TOP")
-            set_alignment_y(alignment_y::top);
-        else if (align_y == "MIDDLE")
-            set_alignment_y(alignment_y::middle);
-        else if (align_y == "BOTTOM")
-            set_alignment_y(alignment_y::bottom);
-        else {
-            gui::out << gui::warning << node.get_location() << ": "
-                     << "Unknown vertical alignment behavior for " << name_ << ": \"" << align_y
-                     << "\"." << std::endl;
-        }
-    }
+    if (const layout_attribute* attr = node.try_get_attribute("alignX"))
+        set_alignment_x(attr->get_value<alignment_x>());
+    if (const layout_attribute* attr = node.try_get_attribute("alignY"))
+        set_alignment_y(attr->get_value<alignment_y>());
+    if (const layout_attribute* attr = node.try_get_attribute("vertexCacheStrategy"))
+        set_vertex_cache_strategy(attr->get_value<vertex_cache_strategy>());
 }
 
 void font_string::parse_shadow_node_(const layout_node& node) {
